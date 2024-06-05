6aa3ba6397a16e7c5ebe11d396d273ae010b716c
brad-craig <134657831+bradacraig@users.noreply.github.com>
2024-06-05 16:42:37 +1200

    start writing delete api

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -18,3 +18,12 @@ export async function addTodo(todo: string) {
   await request.post(rootURL).send(newTask)
   // return Promise.resolve(todo) //TODO implement this
 }
+
+export async function updateTodo(updatedTodo: string, id: number) {
+  await request.patch(`${rootURL}${id}`).send(updatedTodo)
+}
+
+// export async function deleteTodo(todo:string, id:number) {
+//   await request.delete(`${rootURL}${id}`)
+
+// }
