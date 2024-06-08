4a7dcbfbc98dcf6696a112e7f2902d597146d1a2
Brad Craig <brad.a.craig@gmail.com>
2024-06-08 19:05:47 +1200

    added delete function to app, front and back plus button for UI

--- client/components/TodoList.tsx
+++ client/components/TodoList.tsx
@@ -1,16 +1,41 @@
 import { Task } from '../../models/tasks'
+import useDeleteTodo from '../hooks/useDeleteTodo'
 
 interface Props {
   todos: Task[]
 }
 
+// Task {
+//   id: number
+//   task: string
+//   completed: boolean
+// }
+
 export default function TodoList({ todos }: Props) {
+  const deleteTodo = useDeleteTodo()
+
+  const handleDelete = async (
+    id: number,
+    e: { preventDefault: () => void },
+  ) => {
+    e.preventDefault()
+    deleteTodo.mutate(id)
+  }
+
   return (
     <>
       <div>To Do List:</div>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.task}</li>
+          <li key={todo.id}>
+            {todo.task}
+            <button
+              className="delete-btn"
+              onClick={(e) => handleDelete(todo.id, e)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
